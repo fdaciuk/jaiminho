ef26f98a99726d845c2c0bd4ca8b9fab9936e424
Fernando Daciuk <f.daciuk@gmail.com>
2015-08-05 23:21:22 -0300

    Remove unused dependencies

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 var express = require('express');
-var http = require('http');
-var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var port = process.env.PORT || '3000';
 
 var app = express();
-app.set('port', port);
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 require( './routes' )(app);
 require( './modules/middlewares' )(app);
 
-
-http.createServer(app).listen(port, function() {
-  console.log( 'Jaiminho delivering little letters at http://localhost:3000' );
-});
+module.exports = app;
