4dd9aa89d49b2a21e9a3e37b20a675183200717f
Fernando Daciuk <f.daciuk@gmail.com>
2015-08-06 13:44:02 -0300

    Fix controller

--- controllers/cepController.js
+++ controllers/cepController.js
@@ -2,36 +2,36 @@
 var debug = require('debug')('jaiminho:cepController');
 
 module.exports = function(cepModel) {
-  function cepController() {
-    var $public = {};
-    var $private = {};
-
-    $public.get = function get(request, response, next) {
-      var cep = request.params.cep;
-      cepModel.findOne(cep, function(error, data) {
-        if(error)
-          response.status( 404 ).json({ error: 'CEP ' + cep + ' not found' });
-        response.json( data );
-      });
-    };
-
-    $public.create = function create(request, response) {
-      var cep = request.body.cep;
-      cepModel.create(request.body, function(error, data) {
-        if(error)
-          response.status(500).json({ error: 'Internal Server Error' });
-        response.json({ message: cep + ' succesfully inserted!' });
-      });
-    };
-
-    $public.update = function update(request, response) {
-    };
-
-    $public.delete = function del(request, response) {
-    };
-
-    return $public;
-  }
-
-  return cepController();
+  return cepController(cepModel);
 };
+
+function cepController(cepModel) {
+  var $public = {};
+  var $private = {};
+
+  $public.get = function get(request, response, next) {
+    var cep = request.params.cep;
+    cepModel.findOne(cep, function(error, data) {
+      if(error)
+        response.status( 404 ).json({ error: 'CEP ' + cep + ' not found' });
+      response.json( data );
+    });
+  };
+
+  $public.create = function create(request, response) {
+    var cep = request.body.cep;
+    cepModel.create(request.body, function(error, data) {
+      if(error)
+        response.status(500).json({ error: 'Internal Server Error' });
+      response.json({ message: cep + ' succesfully inserted!' });
+    });
+  };
+
+  $public.update = function update(request, response) {
+  };
+
+  $public.delete = function del(request, response) {
+  };
+
+  return $public;
+}
