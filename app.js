6dabbccf66bafa544bd2758b19cd8584f037cca4
Fernando Daciuk <f.daciuk@gmail.com>
2015-09-10 23:05:28 -0300

    Change app to es6

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict';
-var express = require('express');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -12,4 +12,4 @@ app.use(bodyParser.urlencoded({ extended: true }));
 require('./routes')(app);
 require('./modules/middlewares')(app);
 
-module.exports = app;
+export default app;
