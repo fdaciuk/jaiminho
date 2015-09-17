3983bdbcbc90dad924af84178b70f3d09b1f4e44
Fernando Daciuk <f.daciuk@gmail.com>
2015-09-17 07:34:40 -0300

    Tests in es6

--- test/api/cep.test.js
+++ test/api/cep.test.js
@@ -1,15 +1,16 @@
 'use strict';
-require('should');
-var app = require('../../app');
-var request = require('supertest');
-var debug = require('debug')('jaiminho:test');
 
-describe('Test Jaiminho API', function() {
-  it('GET /cep returns `{"message":"Jaiminho CEP API"}`', function(done) {
+let debug = require('debug')('jaiminho:test');
+import app from '../../app';
+import request from 'supertest';
+import 'should';
+
+describe('Test Jaiminho API', () => {
+  it('GET /cep returns `{"message":"Jaiminho CEP API"}`', done => {
     request(app)
       .get('/cep')
-      .end(function(error, response) {
-        var result = response.text;
+      .end((error, response) => {
+        let result = response.text;
         debug(error, result);
         result.should.be.equal('{"message":"Jaiminho CEP API"}');
         done();
